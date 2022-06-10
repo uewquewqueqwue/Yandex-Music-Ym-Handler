86e24360e7c076d704cf51a21b9dee92f00854da
uewquewqueqwueᅠ ᅠ  ᅠ (´°ω°｀) <91084267+uewquewqueqwue@users.noreply.github.com>
2022-06-11 01:43:18 +0300

    update request

--- re_parsing/simple_request.py
+++ re_parsing/simple_request.py
@@ -5,8 +5,8 @@ import aiohttp
 class Request:
     HEADERS = {'User-Agent' : "Magic Browser"}
 
-    def __init__(self, link: str) -> None:
-        self.link = link
+    def __init__(self, url: str) -> None:
+        self.url = url
         self.event_loop = asyncio.get_event_loop()
 
 
@@ -14,10 +14,22 @@ class Request:
         return self.event_loop.run_until_complete(self.request())
 
     async def request(self) -> str:
-        async with aiohttp.ClientSession(headers=self.HEADERS) as session:
-            async with session.get(self.link) as response:
+        async with aiohttp.ClientSession(headers = self.HEADERS) as session:
+            try:
+                async with session.get(self.url) as response:
 
-                if response.status != 200:
-                    print('Error request!')
+                    if response.status == 200:
+                        return await response.text()
+                    
+                    elif response.status == 404: 
+                        print('Трек или альбом не найден, проверьте вышу ссылку '
+                              'на правильность')
+                        exit()
 
-                return await response.text()
+            except aiohttp.ClientConnectionError: 
+
+                return print('Error request!')
+            
+            except Exception as e:
+
+                return print('Unknown error, send her to author', e)
